491d1adee2229789713e29b0e3b378ece82c7a18
João Amaral <amarelo@amaral-air.local>
2025-07-27 22:20:36 +0100

    initial prices

--- frontend/src/components/ServicePrices.js
+++ frontend/src/components/ServicePrices.js
@@ -8,20 +8,49 @@ function ServicePrices() {
           <div className="text-">
             <div className="flex items-end">
               <h2 className="font-bold min-w-fit">Manicure Simples</h2>
-                <div className="h-px w-full mb-1 bg-neutral-400"></div>
-                <p>7.00€</p>
+              <div className="h-px w-full mb-1 bg-neutral-400"></div>
+              <p>7.00€</p>
             </div>
             <p>30 minutos</p>
           </div>
 
           <div className="text-left">
-          <div className="flex items-end">
-          <h2 className="font-bold min-w-fit">Manicure simples com pintura</h2>
-          <div className="h-px w-full mb-1 bg-neutral-400"></div>
-          <p>8.50€</p>
+            <div className="flex items-end">
+              <h2 className="font-bold min-w-fit">Manicure simples com pintura</h2>
+              <div className="h-px w-full mb-1 bg-neutral-400"></div>
+              <p>8.50€</p>
+            </div>
+            <p>30 minutos</p>
           </div>
-          <p>30 minutos</p>
+
+          <div className="text-left">
+            <div className="flex items-end">
+              <h2 className="font-bold min-w-fit">Verniz Gel</h2>
+              <div className="h-px w-full mb-1 bg-neutral-400"></div>
+              <p>15.00€</p>
+            </div>
+            <p>60 minutos</p>
           </div>
+
+          <div className="text-left">
+            <div className="flex items-end">
+              <h2 className="font-bold min-w-fit">1ª Manutenção de Gel sobre a unha </h2>
+              <div className="h-px w-full mb-1 bg-neutral-400"></div>
+              <p>25.00€</p>
+            </div>
+            <p>90 minutos</p>
+          </div>
+
+          <div className="text-left">
+            <div className="flex items-end">
+              <h2 className="font-bold min-w-fit">Manutenção de Gel </h2>
+              <div className="h-px w-full mb-1 bg-neutral-400"></div>
+              <p>20.00€</p>
+            </div>
+            <p>90 minutos</p>
+          </div>
+
+          
 
         </ul>
       </section>
