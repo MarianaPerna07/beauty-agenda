343e6ecd41533557526559a23f8f298e32455a06
marianaperna07 <marianaperna@ua.pt>
2025-08-03 21:11:14 +0100

    first admin page version

---
+++ admin/src/pages/Settings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Typography from "@material-ui/core/Typography";
+
+function SettingsPage() {
+    return (
+        <Typography variant={"h6"} noWrap>
+            Settings Page
+        </Typography>
+    );
+}
+
+export default SettingsPage;
