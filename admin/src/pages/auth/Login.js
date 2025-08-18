7270ee2c7535beed3bb96de0a3ed8a6e574dfa95
jp-amaral <joaopcamaral@gmail.com>
2025-08-18 19:44:31 +0100

    Update admin API endpoint

--- admin/src/pages/auth/Login.js
+++ admin/src/pages/auth/Login.js
@@ -15,7 +15,7 @@ export default function LoginPage() {
             setError("");
             setLoading(true);
             try {
-                const res = await fetch("http://127.0.0.1:5000/auth/google", {
+                const res = await fetch("http://127.0.0.1:5001/auth/google", {
                     method: "POST",
                     headers: { "Content-Type": "application/json" },
                     body: JSON.stringify({ id_token: response.credential }),
